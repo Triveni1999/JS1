bb0bf3216a1872147c51fb2b39f6e71466d6b39d
Triveni1999 <96304543+Triveni1999@users.noreply.github.com>
2022-03-04 17:04:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,112 @@
+// 03/03/2022
+//Decision making
+ //let =10
+// if(a==10){
+     //success
+// } else{
+     //failure
+// }
+
+ // if - else ladder
+ /*let a=-10;
+ if(a<0){
+     console.log("Negative");
+ }else if(a==0){
+     console.log("zero");
+ } else{
+     console.llog("Positive")
+ }*/
+
+ // three or more cond1ition
+ // switch-case statement
+ /*let gender = "m";
+ switch(gender){
+    case"m":
+        console.log("Male");
+        break;
+    case"f":
+         console.log("Female");
+         break;
+    case"T":
+          console.l1og("Trans gender");
+          break;     
+      
+ }*/
+ // Take age as the variable 
+// age < 18 = Child
+// greater than 18 but less than 40 = Adult 
+// age is greater than 40 = Old age
+
+/*let age = 22;
+switch (age) {
+    case age < 18:
+        console.log("child");
+        break;
+    case 18 < age < 40:
+        console.log("adult");
+        break;
+    default:
+        console.log("old age");
+        break;        
+}*/
+// take variable name as day , its possible values are 0 1 2 3 4 5
+// 0 = Monday 
+// 5 : Saturday
+
+/*let day=4;
+switch(day){
+    case 0:
+        console.log("monday");
+        break;
+    case 1:
+        console.log("tuesday");
+        break;
+    case 2:
+        console.log("wedneday");
+        break;
+    case 3:
+        console.log("thursday");
+        break;
+    case 4:
+        console.log("friday");
+        break;
+    case 5:
+        console.log("saturday");
+        break;
+    default:
+        console.log("weekend")  ;
+        break;                     
+}*/
+
+//Loops
+//Iteration stateements
+//loops - for , while, do while,
+/*console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);*/
+
+//3 values
+//Initialization
+// condition
+// Increment/ Decrement
+
+for(let i = 1; i<=10; i++ ){
+    console.log(19*i);
+
+}
+
+// Question : print the table of 19 like 19*1=19
+
+// var a; created variable
+//declaring variable
+//var a=12;
+// we are initial1izing the variabl1e with the value of 12
+
+ 
